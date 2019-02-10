c5d0682f95a65cafa8654f6ea8c1146c991bd60e
potash <35871896+potahyox@users.noreply.github.com>
2019-02-11 00:23:35 +0900

    Update Point.h

--- Point.h
+++ Point.h
@@ -7,7 +7,7 @@ typedef struct _point
 	int ypos;
 } Point;
 
-void SetPointPos(Point * ppos, int xpos);
+void SetPointPos(Point * ppos, int xpos, int ypos);
 void ShowPointPos(Point * ppos);
 int PointComp(Point * pos1, Point * pos2);
 
