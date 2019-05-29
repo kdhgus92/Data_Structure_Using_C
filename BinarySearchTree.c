cea23539257b1b68e93da9b0be14439c17e29255
potash <35871896+potahyox@users.noreply.github.com>
2019-05-29 22:39:41 +0900

    Create BinarySearchTree.c

---
+++ BinarySearchTree.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include "BinaryTree2.h"
+#include "BinarySearchTree.h"
+
+void BSTMakeAndInit(BTreeNode ** pRoot)
+{
+	*pRoot = NULL;
+}
+
+BSTData BSTGetNodeData(BTreeNode * bst)
+{
+	return GetData(bst);
+}
+
+void BSTInsert(BTreeNode ** pRoot, BSTData data)
+{
+	BTreeNode * pNode = NULL;
+	BTreeNode * cNode = *pRoot;
+	BTreeNode * nNode = NULL;
+
+	while (cNode != NULL)
+	{
+		if (data == GetData(cNode))
+			return; //데이터 키의 중복 허용하지 않음
+
+		pNode = cNode;
+
+		if (GetData(cNode) > data)
+			cNode = GetLeftSubTree(cNode);
+		else
+			cNode = GetRightSubTree(cNode);
+	}
+
+	nNode = MakeBTreeNode();
+	SetData(nNode, data);
+
+	if (pNode != NULL)
+	{
+		if (data < GetData(pNode))
+			MakeLeftSubTree(pNode, nNode);
+		else
+			MakeRightSubTree(pNode, nNode);
+	}
+	else
+	{
+		*pRoot = nNode;
+	}
+}
+
+BTreeNode * BSTSearch(BTreeNode * bst, BSTData target)
+{
+	BTreeNode * cNode = bst;
+	BSTData cd;
+
+	while (cNode != NULL)
+	{
+		cd = GetData(cNode);
+
+		if (target == cd)
+			return cNode;
+		else if (target < cd)
+			cNode = GetLeftSubTree(cNode);
+		else
+			cNode = GetRightSubTree(cNode);
+	}
+
+	return NULL; //탐색대상이 저장되어 있지 않음.
+}
