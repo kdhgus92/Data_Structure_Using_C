b1b23d82fb86ff054179c55f5afedb2dea109592
potash <35871896+potahyox@users.noreply.github.com>
2019-05-30 00:28:12 +0900

    Create BinaryTree3.h

---
+++ BinaryTree3.h
@@ -0,0 +1,37 @@
+#ifndef __BINARY_TREE3_H__
+#define __BINARY_TREE3_H__
+
+typedef int BTData;
+
+typedef struct _bTreeNode
+{
+	BTData data;
+	struct _bTreeNode * left;
+	struct _bTreeNode * right;
+} BTreeNode;
+
+BTreeNode * MakeBTreeNode(void);
+BTData GetData(BTreeNode * bt);
+void SetData(BTreeNode * bt, BTData data);
+
+BTreeNode * GetLeftSubTree(BTreeNode * bt);
+
+void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub);
+void MakeRightSubTree(BTreeNode * main, BTreeNode * sub);
+
+typedef void VisitFuncPtr(BTData data);
+
+void PreorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void InorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void PostorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+
+//8-1 연습문제 추가 
+void DeleteTree(BTreeNode * bt);
+
+//BinaryTree2 확장
+BTreeNode * RemoveLeftSubTree(BTreeNode * bt);
+BTreeNode * RemoveRightSubTree(BTreeNode * bt);
+void ChangeLeftSubTree(BTreeNode * main, BTreeNode * sub);
+void ChangeRightSubTree(BTreeNode * main, BTreeNode * sub);
+
+#endif
