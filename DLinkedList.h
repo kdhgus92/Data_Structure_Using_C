e8b9f6db11fa7d2bcfe0612cc7e108dc82e68ea5
potash <35871896+potahyox@users.noreply.github.com>
2019-06-09 17:38:49 +0900

    Update DLinkedList.h

--- DLinkedList.h
+++ DLinkedList.h
@@ -1,10 +1,13 @@
 #ifndef __D_LINKED_LIST_H__
 #define __D_LINKED_LIST_H__
 
+//#include "Slot2.h"
+
 #define TRUE 1
 #define FALSE 0
 
 typedef int LData;
+//typedef slot LData;
 
 typedef struct _node
 {
